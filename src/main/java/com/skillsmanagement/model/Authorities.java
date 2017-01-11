becb64ad468704693ec69bf9f420708989af446f
Olegste <dolina.oleg@gmail.com>
2017-01-11 18:36:12 +0200

    hibernate

--- src/main/java/com/skillsmanagement/model/Authorities.java
+++ src/main/java/com/skillsmanagement/model/Authorities.java
@@ -1,18 +1,14 @@
 package com.skillsmanagement.model;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
-@Table(name = "authorities")
 public class Authorities {
 
 	@Id
 	@GeneratedValue
-	@Column(name = "authoritiesid")
 	private int authoritiesId;
 	private String username;
 	private String authority;
